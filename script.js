69f9a8a4d963464ecb17bcef44417a6091c82444
kenneth walker <kdwalker91@gmail.com>
2017-06-08 16:00:34 -0500

    removed about me section for now.

--- script.js
+++ script.js
@@ -3,14 +3,16 @@
 
 /*When user scrolls past a set point the progress
 bars will animate from left to right*/
-$(window).scroll(function () {
-	var y_scroll_pos = window.pageYOffset;
-	var scroll_pos_test = 150;
-	// set to whatever you want it to be
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var y_scroll_pos = window.pageYOffset;
+		var scroll_pos_test = 150;
+		// set to whatever you want it to be
 
-	if (y_scroll_pos > scroll_pos_test) {
-		$(".bar1").animate({
-			width: "70%"
-		}, 2500);
-	}
+		if (y_scroll_pos > scroll_pos_test) {
+			$(".bar1").animate({
+				width: "70%"
+			}, 2500);
+		}
+	});
 });
