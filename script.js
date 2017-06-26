439a224e31add4ac55669eb0fc52b8d60939228c
kenneth walker <kdwalker91@gmail.com>
2017-06-26 05:07:07 -0500

    updated cosmetics

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/*When user scrolls past a set point the progress
-bars will animate from left to right
-$(document).ready(function () {
-<<<<<<< HEAD
-	$(window).scroll(function () {
-		var y_scroll_pos = window.pageYOffset;
-		var scroll_pos_test = 1;
-		// set to whatever you want it to be
-=======
-    $(window).scroll(function () {
-        var y_scroll_pos = window.pageYOffset;
-        var scroll_pos_test = 20;
-        // set to whatever you want it to be
->>>>>>> f62cd51de5e6c5af8047821ae8bb4c4385022fa1
-
-        if (y_scroll_pos > scroll_pos_test) {
-            $(".bar1").animate({
-                width: "70%"
-            }, 2500);
-        }
-    });
-});
-*/
 $(document).ready(function () {
 	$(".bars").delay(1500).animate({
 		width: "70%"
