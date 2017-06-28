0d6205d689141e64a819be71bb8c5f36a7089590
kenneth walker <kdwalker91@gmail.com>
2017-06-28 02:46:18 -0500

    updated a lot of stuff mainly well too much to mention

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-$(document).ready(function () {
-	$(".bars").delay(1500).animate({
-		width: "90%"
-	}, 1500);
+//$(document).ready(function () {
+//	$(".bars").delay(1500).animate({
+//		width: "90%"
+//	}, 1500);
+//});
+
+$("button").click(function() {
+  $( ".description, .relevant-courses " ).toggle( "slow" );
 });
